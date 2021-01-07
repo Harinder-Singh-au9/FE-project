76a1847c828f1152b27704480388d145ca78d84a
Harinder Singh <singh.harinder45@gmail.com>
2021-01-07 22:14:52 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./css/header.css"
+import Logo from "./images/logo.PNG"
+const Header = () => {
+    return ( <
+        div class = "header" >
+        <
+        img class = "logo"
+        src = { Logo }
+        alt = "loding issue" / >
+        <
+        /div>
+    )
+
+}
+
+export default Header;
