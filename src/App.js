2256ef64b6a23f8046bc2442ba941587a4d9e9d5
Harinder Singh <singh.harinder45@gmail.com>
2021-01-11 11:55:49 +0530

    routing with navbar and home

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar.js"
 import "./components/css/main.css"
 import Home from "./components/Home.js"
-
+import { BrowserRouter, Route } from "react-router-dom";
+import Profile from "./components/profile.js"
 // import MainPageBeforeLogin from "./components/MainPageBeforeLogin";
 export default class App extends Component {
     render() {
         return ( <
+            BrowserRouter >
+            <
             div className = "App" >
 
             <
             Navbar / >
             <
-            /div>
+            Route exact path = "/"
+            component = { Home }
+            /> <
+            Route path = "/profile"
+            component = { Profile }
+            /> <
+            /div> <
+            /BrowserRouter>
         );
     }
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React from "react";
 
 const Profile = () => {
-    return ( <
-        h1 > Proile < /h1>
-    )
-}
+    return <h1 className = "center" > Proile < /h1>;
+};
 export default Profile;
